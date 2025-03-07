143021f165df37a4397c62016a526cef85f35ec5
Sviatoslav <svyatoslavdjs@gmail.com>
2025-03-07 22:57:28 +0200

    Make a sectioon catalog

--- src/js/script.js
+++ src/js/script.js
@@ -10,9 +10,38 @@ $(document).ready(function () {
                 breakpoint: 768,
                 settings: {
                     dots: true,
-                    arrows: folse
+                    arrows: false
                 }
             }
         ]
     });
-}) 
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
+// var slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controlsText: [
+//         '<img src="../icons/arrow_left.png"></img>',
+//         '<img src="../icons/arrow_right.png"></img>'
+//     ]
+// });
