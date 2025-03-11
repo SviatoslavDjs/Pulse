ff97a1c25ce4a2c1f99342a843f9d4fe0f748fa5
Sviatoslav <svyatoslavdjs@gmail.com>
2025-03-11 11:47:12 +0200

    Make a modal window

--- src/js/script.js
+++ src/js/script.js
@@ -34,6 +34,21 @@ $(document).ready(function () {
     };
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow')
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__desr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 });
 // var slider = tns({
 //     container: '.carousel__inner',
