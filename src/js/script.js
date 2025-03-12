1221609aa2f1166e7c1b51b97becb7b31359ddc7
Sviatoslav <svyatoslavdjs@gmail.com>
2025-03-12 22:44:28 +0200

    Install and using in projects phpmailer

--- src/js/script.js
+++ src/js/script.js
@@ -80,6 +80,26 @@ $(document).ready(function () {
     valideForms('#consultation form');
     valideForms('#order form');
     $('input[name=phone]').mask("+3 (999) 999-99-99")
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        };
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
 });
 // var slider = tns({
 //     container: '.carousel__inner',
