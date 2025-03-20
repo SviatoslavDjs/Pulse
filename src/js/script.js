28019dc5891393a66bf8a036fdba70c5e0663bf3
Sviatoslav <svyatoslavdjs@gmail.com>
2025-03-20 22:48:32 +0200

    Add animation and scroll up

--- src/js/script.js
+++ src/js/script.js
@@ -100,6 +100,47 @@ $(document).ready(function () {
         return false;
     });
 
+    //scroll and pageup
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        }
+        else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+    wow = new WOW(
+        {
+            boxClass: 'wow',      // default
+            animateClass: 'animate__animated', // default
+            offset: 0,          // default
+            mobile: true,       // default
+            live: true        // default
+        }
+    )
+    wow.init();
+
 });
 // var slider = tns({
 //     container: '.carousel__inner',
